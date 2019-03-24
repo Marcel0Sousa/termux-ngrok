60ffdb6ba92be75d6ba20754f5935d8c10d9c783
tchelospy <tchelospy@gmail.com>
2019-03-24 18:33:15 -0300

    Atualização do script

---
+++ termux-ngrok.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#sudo apt update && upgrade -y
+#sudo apt install -y wget figlet
+
+figlet NGROK
+echo
+echo "Downloading Termux-ngrok"
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "Arquitetura desconhecida"
+esac
+
+#wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip"
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+chmod 700 /data/data/com.termux/files/usr/bin/ngrok
+rm ngrok
+#chmod +x ngrok
+echo "Exemplos de comando = ngrok http 80"
+
+
+
